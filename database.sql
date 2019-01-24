3c65db731d79c0b470589bebad85d1c52e546158
evilmurries <cpiwarski00@gmail.com>
2019-01-23 21:18:41 -0800

    full roster

--- database.sql
+++ database.sql
@@ -16,13 +16,24 @@ CREATE TABLE user_levels (
     grade_access VARCHAR(25) NOT NULL
 );
 
+CREATE TABLE classes (
+    class_id INT PRIMARY KEY AUTO_INCREMENT,
+    class_name VARCHAR(50),
+    class_time DECIMAL (4,2),
+    classroom_number INT(50),
+    start_date DATE NOT NULL,
+    end_date DATE NOT NULL,
+    class_units DECIMAL(3,2)
+);
+
 CREATE TABLE accounts (
     user_id INT PRIMARY KEY AUTO_INCREMENT,
     user_name VARCHAR(50) NOT NULL,
     user_password VARCHAR(50) NOT NULL,
     user_level INT NOT NULL,
     class_id INT,
-    CONSTRAINT user_level_fk FOREIGN KEY (user_level) REFERENCES user_levels (account_level)
+    CONSTRAINT user_level_fk FOREIGN KEY (user_level) REFERENCES user_levels (account_level),
+    CONSTRAINT class_id_fk FOREIGN KEY (class_id) REFERENCES classes(class_id)
 );
 
 CREATE TABLE grades (
@@ -34,15 +45,6 @@ CREATE TABLE grades (
     CONSTRAINT user_id_fk FOREIGN KEY (user_id) REFERENCES accounts (user_id)
 );
 
-CREATE TABLE classes (
-    class_id INT PRIMARY KEY AUTO_INCREMENT,
-    class_name VARCHAR(50),
-    class_time DECIMAL (4,2),
-    classroom_number INT(50),
-    start_date DATE NOT NULL,
-    end_date DATE NOT NULL,
-    class_units DECIMAL(3,2)
-);
 
 -- insert data
 
@@ -73,6 +75,11 @@ INSERT INTO classes
 VALUES
     ("English", 9.50, 3, "2019-01-05", "2019-4-25", 5.0);
     
+INSERT INTO classes
+    (class_name, class_time, classroom_number, start_date, end_date, class_units)
+VALUES
+    ("Databases", 9.50, 1, "2019-01-05", "2019-4-25", 4.5);
+    
 -- create administrator
 INSERT INTO accounts
     (user_name, user_password, user_level)
@@ -80,8 +87,95 @@ VALUES
     ("admin", "admin", 3);
     
 -- create teachers
+INSERT INTO accounts
+    (user_name, user_password, user_level, class_id)
+VALUES
+    ("teacher1", "teacher1", 2, 1);
+    
+INSERT INTO accounts
+    (user_name, user_password, user_level, class_id)
+VALUES
+    ("teacher2", "teacher2", 2, 2);
+    
+INSERT INTO accounts
+    (user_name, user_password, user_level, class_id)
+VALUES
+    ("teacher3", "teacher3", 2, 3);
 
 -- create students
+INSERT INTO accounts
+    (user_name, user_password, user_level, class_id)
+VALUES
+    ("student1", "student1", 1, 1);
+
+INSERT INTO accounts
+    (user_name, user_password, user_level, class_id)
+VALUES
+    ("student2", "student2", 1, 1);
     
+INSERT INTO accounts
+    (user_name, user_password, user_level, class_id)
+VALUES
+    ("student3", "student3", 1, 1);
+    
+INSERT INTO accounts
+    (user_name, user_password, user_level, class_id)
+VALUES
+    ("student4", "student4", 1, 1);
+    
+INSERT INTO accounts
+    (user_name, user_password, user_level, class_id)
+VALUES
+    ("student5", "student5", 1, 1);
+    
+INSERT INTO accounts
+    (user_name, user_password, user_level, class_id)
+VALUES
+    ("student6", "student6", 1, 2);
+    
+INSERT INTO accounts
+    (user_name, user_password, user_level, class_id)
+VALUES
+    ("student7", "student7", 1, 2);
+    
+INSERT INTO accounts
+    (user_name, user_password, user_level, class_id)
+VALUES
+    ("student8", "student8", 1, 2);
+    
+INSERT INTO accounts
+    (user_name, user_password, user_level, class_id)
+VALUES
+    ("student9", "student9", 1, 2);
+    
+INSERT INTO accounts
+    (user_name, user_password, user_level, class_id)
+VALUES
+    ("student10", "student10", 1, 2);
+    
+INSERT INTO accounts
+    (user_name, user_password, user_level, class_id)
+VALUES
+    ("student11", "student11", 1, 3);
+    
+INSERT INTO accounts
+    (user_name, user_password, user_level, class_id)
+VALUES
+    ("student12", "student12", 1, 3);
+    
+INSERT INTO accounts
+    (user_name, user_password, user_level, class_id)
+VALUES
+    ("student13", "student13", 1, 3);
+    
+INSERT INTO accounts
+    (user_name, user_password, user_level, class_id)
+VALUES
+    ("student14", "student14", 1, 3);
+    
+INSERT INTO accounts
+    (user_name, user_password, user_level, class_id)
+VALUES
+    ("student15", "student15", 1, 3);
     
     
