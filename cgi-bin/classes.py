ebed0690c87b23930abc627a0918f1cb827b750d
weiss21 <34324418+weiss21@users.noreply.github.com>
2019-01-27 17:42:06 -0800

    Update classes.py
    Added cookie. Still doesn't print.

--- cgi-bin/classes.py
+++ cgi-bin/classes.py
@@ -4,24 +4,36 @@
 # 26 January 2019
 # CST363 Databases Online
 # this file must be in the /cgi-bin/ directory of the server
-
+from os import environ
+import string
 import cgitb, cgi
 import mysql.connector
 cgitb.enable()
 
-cgitb.enable()
 form = cgi.FieldStorage()
-username = form['login'].value
+
+cookies = environ['HTTP_Cookie']
+
+def find_between(words, first, last):
+  try:
+    start = words.index(first) + len(first)
+    end = words.index(last,start)
+    return words[start:end]
+  except  ValueError:
+    return ""
+
+username = find_between(cookies,"username=",";")
+password = find_between(cookies,"password=",";")
 
 # HTML Header
 print('Content-Type: text/html')
 print()
-print('<html><title>CLASSES</title>><body>')
+print('<html><title>CLASSES</title><body>')
 
 try:
     cnx = mysql.connector.connect(user='root',
-                                password='Mercy1988!',
-                                database='cst363',
+                                password='root',
+                                database='cms',
                                 host='127.0.0.1')
 
     cursor1 = cnx.cursor()
@@ -34,18 +46,36 @@ student_query = 'SELECT a.user_name, classes, \
 FROM accounts a JOIN classes c ON a.user_id = g.user_id \
 WHERE user_name = %s;'
 
-teacher_query = 'SELECT a.user_name, class,  \
+teacher_query = 'SELECT a.user_name, classes,  \
 FROM accounts a JOIN classes c ON a.user_id = g.user_id \
 WHERE class_id = (SELECT class_id FROM accounts WHERE user_name = %s);'
 
 # determine user level 
-ul_query = 'SELECT user_level FROM accounts'
+ul_query = 'SELECT user_level FROM accounts WHERE user_name = \'%s\'' %username
 
 cursor1.execute(ul_query)
-result = cursor.fetchone()
+fetch = cursor1.fetchone()
+test = str(fetch)
+result = find_between(test,"(",",")
+
+
 
 print('<table border="1"><tr><th>Student Name</th><th>Classes</th><th>')
 
+if result == "1":
+    cursor2.execute(student_query, (username,))
+    print('Here is your classes:')
+    grade = cursor2.fetchone()
+    print ('<tr><td>%s <td>%s <td>%s </tr>' % classes)
+elif result == "2":
+    cursor2.execute(teacher_query, (username,))
+    print("These are your student's classes:")
+    grades = cursor2.fetchone()
+    while grades is not None:
+        print ('<tr><td>%s <td>%s <td>%s </tr>' % classes)
+        grades = cursor2.fetchone()
+else:
+    print('Sorry, you do not have access to view classes')
 
 print('<br>')
 print('</table>')
