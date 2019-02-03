15e86c049bb7c4a6e41fc1296134fdc9e82b34f2
evilmurries <cpiwarski00@gmail.com>
2019-02-03 14:14:49 -0800

    database warehouse

--- olap_db.sql
+++ olap_db.sql
@@ -11,12 +11,16 @@ USE cmsdw;
 CREATE TABLE fact_table (
     time_id INT PRIMARY KEY AUTO_INCREMENT,
     login_date DATE NOT NULL,
+    user_id INT NOT NULL,
     user_level INT NOT NULL,
     class_id INT,
-    grade INT
+    grade INT,
+    CONSTRAINT user_level_fk FOREIGN KEY (user_level) REFERENCES user_levels (account_level),
+    CONSTRAINT class_id_fk FOREIGN KEY (class_id) REFERENCES classes (class_id),
+    CONSTRAINT user_id_fk FOREIGN KEY (user_id) REFERENCES user_account (user_id)
 );
 
-CREATE TABLE clases(
+CREATE TABLE classes (
     class_id INT PRIMARY KEY AUTO_INCREMENT,
     class_name VARCHAR(50) NOT NULL,
     classroom_number INT(50) NOT NULL,
@@ -25,9 +29,15 @@ CREATE TABLE clases(
     class_units DECIMAL(3,2) NOT NULL
 );
 
-CREATE TABLE user_account(
+CREATE TABLE user_account (
     user_id INT PRIMARY KEY,
     user_name VARCHAR(50) NOT NULL,
     user_password VARCHAR(50) NOT NULL
+);
+
+CREATE TABLE user_levels (
+    account_level INT PRIMARY KEY,
+    level_name VARCHAR(50) NOT NULL,
+    grade_access VARCHAR(25) NOT NULL 
 );
 
